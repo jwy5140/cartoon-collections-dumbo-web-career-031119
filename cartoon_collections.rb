c22c28812214f9212dcc3f7ff82daa87e9dd6f6b
Jeffrey Yu <jwy5140@gmail.com>
2019-02-21 03:16:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(element)
-  summon = element.collect(|x| (x.capitalize).concat("!"))
+  summon = element.collect{|x| (x.capitalize).concat("!")}
 end
 
 def long_planeteer_calls# code an argument here
