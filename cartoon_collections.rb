5520fa4c0506b383f8820a1d17f0fbe64eab9b83
Jeffrey Yu <jwy5140@gmail.com>
2019-02-21 03:24:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(element)
   summon = element.collect{|x| (x.capitalize).concat("!")}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(arr)
+  
 end
 
 def find_the_cheese# code an argument here
